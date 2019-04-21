ca1e423573ee28465aee4a13793a57d9fd716ac8
Matheus Henrique <matheus.justino@ccc.ufcg.edu.br>
2019-04-21 11:42:43 -0300

    Atualizando o form

--- icc-app/src/App.js
+++ icc-app/src/App.js
@@ -32,63 +32,98 @@ class App extends Component {
               <Col>
 
               <FormGroup>
-                <Label for="avaliacao1">Seu aproveitamento da aula</Label>
-                <Input type="select" name="select" id="avaliacao1">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Row>
+                  <Col className="mt-2 esquerda">
+                    <Label for="avaliacao1">Seu aproveitamento da aula</Label>
+                  </Col>
+
+                  <Col className="col-4 mt-2">
+                    <Input type="select" name="select" id="avaliacao1">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </Row>
               </FormGroup>
 
               <FormGroup>
-                <Label for="avaliacao2">Explicação do conteúdo</Label>
-                <Input type="select" name="select" id="avaliacao2">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Row>
+                  <Col className="mt-2 esquerda">
+                    <Label for="avaliacao2">Explicação do conteúdo</Label>
+                  </Col>
+
+                  <Col className="col-4 mt-2">
+                    <Input type="select" name="select" id="avaliacao2">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </Row>
               </FormGroup>
 
               <FormGroup>
-                <Label for="avaliacao3">Material da aula</Label>
-                <Input type="select" name="select" id="avaliacao3">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Row>
+                  <Col className="mt-2 esquerda">
+                    <Label for="avaliacao3">Material da aula</Label>
+                  </Col>
+
+                  <Col className="col-4 mt-2">
+                    <Input type="select" name="select" id="avaliacao3">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </Row>
               </FormGroup>
 
               <FormGroup>
-                <Label for="avaliacao4">Avaliação geral da aula</Label>
-                <Input type="select" name="select" id="avaliacao4">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Row>
+                  <Col className="mt-2 esquerda">
+                    <Label for="avaliacao4">Avaliação geral da aula</Label>
+                  </Col>
+
+                  <Col className="col-4 mt-2">
+                    <Input type="select" name="select" id="avaliacao4">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </Input>
+                  </Col>
+                </Row>
               </FormGroup>
 
               </Col>
 
-              <Col>
+              <Col className="col-lg-6 col-sm-12">
 
                 <FormGroup>
-                  <Label for="textoAluno">Dica(s) para melhorar a aula</Label>
-                  <Input type="textarea" name="text" id="textoAluno" />
+                  <Row>
+                    <Col className="mt-2 esquerda">
+                      <Label for="textoAluno">Dica(s) para melhorar a aula</Label>
+                    </Col>
+
+                    <Col className="mt-2 col-12">
+                      <Input type="textarea" name="text" id="textoAluno" />
+                    </Col>
+                  </Row>
                 </FormGroup>
               
               </Col>
           
             </Row>
 
-            <Button className="mb-2" color="primary">Enviar</Button>  
+            <Button className="direita my-2" color="primary">Enviar</Button>  
 
           </Form>
 
