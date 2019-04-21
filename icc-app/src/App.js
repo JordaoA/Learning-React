ec727752e14503b5c086139caec45cab1b7ed5bb
Matheus Henrique <matheus.justino@ccc.ufcg.edu.br>
2019-04-20 21:14:44 -0300

    Atualizando o form

--- icc-app/src/App.js
+++ icc-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Nav, Navbar, Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Navbar, Container, Row, Col, Button, Form, FormGroup, Label, Input} from 'reactstrap';
 
 class App extends Component {
   render() {
