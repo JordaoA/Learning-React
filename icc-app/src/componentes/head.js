eec64abf6ac67421b4e860dde1a6ea9d7bb48f03
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-04-23 16:17:19 -0300

    Dividindo em componentes

--- icc-app/src/componentes/head.js
+++ icc-app/src/componentes/head.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Navbar, Label} from 'reactstrap';
+import { Navbar, Label } from 'reactstrap';
+
 
 class Topo extends Component {
-    
-    render() {
-        return (
-            <div>
-                
-                <Navbar color="dark" expand="md">
-                  <div className="container">
-                    <div className="row col-12">
-  
-                      <div className="col-lg-6 col-md-6 col-sm-12">
-                        <Label className="text-light">
-                          Formulário de Avaliação de Aula
-                        </Label>
-                      </div>
-  
-                      <div className="col-lg-3 offset-lg-3 col-md-3 offset-md-2 col-sm-12">
-                        <input className="form-inline" placeholder="Sua Matrícula"></input>
-                      </div>
-                    
-                    </div>
-                  </div>
-                </Navbar>
-                
-            </div>
-        );
-    }
-  }
 
-  export default Topo;
-  
+  render() {
+    return (
+      <div>
+
+        <Navbar color="dark" expand="md">
+          <div className="container">
+            <div className="row col-12">
+
+              <Titulo></Titulo>
+
+              <Matricula></Matricula>
+
+            </div>
+          </div>
+        </Navbar>
+
+      </div>
+    );
+  }
+}
+
+class Titulo extends Component {
+  render() {
+    return (
+
+      <div className="col-lg-6 col-md-6 col-sm-12">
+        <Label className="text-light">
+          Formulário de Avaliação de Aula
+        </Label>
+      </div>
+
+    );
+  }
+}
+
+class Matricula extends Component {
+  render() {
+    return (
+
+      <div className="col-lg-3 offset-lg-3 col-md-3 offset-md-2 col-sm-12">
+        <input className="form-inline" placeholder="Sua Matrícula"></input>
+      </div>
+
+    );
+  }
+}
+
+export default Topo;
