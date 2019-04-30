defe4369698a6fd47e55d2f5a66e98999bf4985d
JordaoA <andre.nascimento@ccc.ufcg.edu.br>
2019-04-30 12:51:28 -0300

    Add script to install dependencies

---
+++ icc-app/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+npm install bootstrap
+npm install chart.js
+npm install react
+npm install react-chartjs-2
+npm install react-dom
+npm install react-router
+npm install react-router-dom
+npm install reactstrap
+
