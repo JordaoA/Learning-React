8ae29035216dbea0e47cc14e46a804fde6caaf5c
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-05-07 20:36:17 -0200

    Metodo POST em construçao

--- icc-app/make.sh
+++ icc-app/make.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+npm install axios
 npm install bootstrap
 npm install chart.js
+npm install nodemon
 npm install react
 npm install react-chartjs-2
 npm install react-dom
 npm install react-router
 npm install react-router-dom
 npm install reactstrap
-npm install express
